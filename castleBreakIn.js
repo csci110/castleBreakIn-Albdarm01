cb73020e9251c67642d60b2f045f0cbc4acd792d
Albdarm <albdarm@dewv.edu>
2018-10-01 13:25:31 +0000

    Updates via sync script

--- castleBreakIn.js
+++ castleBreakIn.js
@@ -16,7 +16,7 @@ new Wall(0, 0, "A spooky castle wall", "castle.png");
 
 let leftWall = new Wall(0, 200, "Left side wall", "wall.png");
 
-let rightWall = new Wall (48, 200, "Right side wall", "wall.png");
+let rightWall = new Wall(game.displayWidth - 48, 200, "Right side wall", "wall.png");
 
 class princess extends Sprite {
     constructor() {
@@ -32,8 +32,66 @@ class princess extends Sprite {
         this.accelerateOnBounce = false;
         this.defineAnimation("left", 9, 11);
         this.defineAnimation("right", 3, 5);
+        this.lives = 3;
+    }
+    handleLeftArrowKey() {
+        this.playAnimation("left");
+        this.angle = 180;
+        this.speed = this.speedWhenWalking;
+    }
+    handleRightArrowKey() {
+        this.playAnimation("right");
+        this.angle = 0;
+        this.speed = this.speedWhenWalking;
+    }
+    handleCollision(otherSprite) {
+        // Horizontally, Ann's image file is about one-third blank, one-third Ann, and         // one-third blank.
+        // Vertically, there is very little blank space. Ann's head is about one-fourth         // the height.
+        // The other sprite (Ball) should change angle if:
+        // 1. it hits the middle horizontal third of the image, which is not blank, AND
+        // 2. it hits the upper fourth, which is Ann's head.
+        let horizontalOffset = this.x - otherSprite.x;
+        let verticalOffset = this.y - otherSprite.y;
+        if (Math.abs(horizontalOffset) < this.width / 3 &&
+            verticalOffset > this.height / 4) {
+            // The new angle depends on the horizontal difference between sprites.
+            otherSprite.angle = 90 + 2 * horizontalOffset;
+        }
+        return false;
+    }
+    handleFirstGameLoop() {
+        // Set up a text area to display the number of lives remaining.
+        this.livesDisplay = game.createTextArea(3, 20);
+        this.updateLivesDisplay();
+        
+    }
+    updateLivesDisplay() {
+        game.writeToTextArea(this.livesDisplay, "Lives = " + this.lives);
     }
 }
-let ann = new princess;
+let ann = new princess();
 
+class Ball extends Sprite {
+    constructor(x, y, name, image) {
+        super();
+        this.x = 390;
+        this.y = 500;
+        this.setImage("ball.png");
+        this.height = 48;
+        this.width = 48;
+        this.name = Ball;
+        this.defineAnimation("spin", 0, 12);
+        this.speed = 40;
+        this.angle = 50 + Math.random() * 80;
 
+    }
+    handlGameLoop() {
+        this.speed = 40;
+        if (this < 200) {
+            this.speed = 40;
+        }
+
+    }
+
+}
+new Ball(360, 90, "ball", "ball.png");
